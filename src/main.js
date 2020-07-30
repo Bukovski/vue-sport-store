e8bb550c9140b224a91c2c65a0a0487df964e3a0
Bukovski <kmanankov@bk.ru>
2020-07-30 14:01:00 +0300

    add: category

--- src/main.js
+++ src/main.js
@@ -7,9 +7,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "font-awesome/css/font-awesome.min.css";
 
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
