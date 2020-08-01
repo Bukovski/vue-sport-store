0f2f5bd1d7a844fa3e4d4549b1b02e544f7c8ea0
Bukovski <kmanankov@bk.ru>
2020-08-01 07:30:00 +0300

    add: cart summary to store page

--- src/components/Store.vue
+++ src/components/Store.vue
@@ -3,6 +3,7 @@
     <div class="row">
       <div class="col bg-dark text-white">
         <a class="navbar-brand">SPORTS STORE</a>
+        <CartSummary/>
       </div>
     </div>
     <div class="row">
@@ -23,9 +24,10 @@
 <script>
 	import ProductList from "./ProductList";
 	import CategoryControls from "./CategoryControls";
+	import CartSummary from "./CartSummary";
 	
 	
 	export default {
-		components: { ProductList, CategoryControls }
+		components: { ProductList, CategoryControls, CartSummary }
 	}
 </script>
